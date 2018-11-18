22d1412e8bb65ae022f7d658e122d7537a7dc5c6
miragessee <miragessee@hotmail.com>
2018-11-18 15:43:06 +0300

    hepsi

--- app/src/main/java/com/grup22/model/Data.java
+++ app/src/main/java/com/grup22/model/Data.java
@@ -4,14 +4,14 @@ package com.grup22.model;
  * Created by hp-pc on 18.11.2018.
  */
 
-public class data {
+public class Data {
     String question;
     String trueAnswer;
     String falseAnswer1;
     String falseAnswer2;
     String falseAnswer3;
 
-    public data(String question, String trueAnswer, String falseAnswer1, String falseAnswer2, String falseAnswer3) {
+    public Data(String question, String trueAnswer, String falseAnswer1, String falseAnswer2, String falseAnswer3) {
         this.question = question;
         this.trueAnswer = trueAnswer;
         this.falseAnswer1 = falseAnswer1;
@@ -19,4 +19,23 @@ public class data {
         this.falseAnswer3 = falseAnswer3;
     }
 
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getTrueAnswer() {
+        return trueAnswer;
+    }
+
+    public String getFalseAnswer1() {
+        return falseAnswer1;
+    }
+
+    public String getFalseAnswer2() {
+        return falseAnswer2;
+    }
+
+    public String getFalseAnswer3() {
+        return falseAnswer3;
+    }
 }
